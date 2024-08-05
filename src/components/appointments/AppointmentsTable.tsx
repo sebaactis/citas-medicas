67b9935205a334ff34da89b5d92f9adbd7d757c0
sebaactis <sebaactis@gmail.com>
2024-08-05 12:06:15 -0300

    prueba url api v2

--- src/components/appointments/AppointmentsTable.tsx
+++ src/components/appointments/AppointmentsTable.tsx
@@ -34,7 +34,7 @@ export default function AppointmentsTable() {
 
         setIsLoading(true);
         try {
-            const response = await fetch(`https://hospitalityapp.netlify.app/api/appointment/appointments?page=${page}&limit=${limit}&pagination=${pagination}`)
+            const response = await fetch(`http://hospitalityapp.netlify.app/api/appointment/appointments?page=${page}&limit=${limit}&pagination=${pagination}`)
             const data = await response.json();
             setAppointments(data.appointments);
             setTotalPages(data.totalPages);
