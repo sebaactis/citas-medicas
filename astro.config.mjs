1766bd629ffec2f0c7cd801f142f1e42439aeeae
sebaactis <sebaactis@gmail.com>
2024-08-05 10:27:24 -0300

    netlify adaptar ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   integrations: [
     react(), 
     tailwind({
