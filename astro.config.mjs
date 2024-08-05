36b50418fff27920a9c3085f7ebdbf2ae8d7490d
sebaactis <sebaactis@gmail.com>
2024-08-05 15:05:26 -0300

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: vercel(),
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })]
